5eb684c169e314130b5b6eb181310519d87ab5a8
Joanna Lin <linjoanna31@gmail.com>
2022-06-01 22:52:43 +1000

    Added week 1 content

---
+++ tut01/face.c
@@ -0,0 +1,17 @@
+// A program that prints out the following face
+// ~ ~
+// 0 0
+//  o 
+// \_/
+// W17A, June 2022
+
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+    
+    return 0;
+}
---
+++ tut01/hello_world.c
@@ -0,0 +1,10 @@
+// Basic Hello World program    
+// W17A, June 2022
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    
+    return 0;
+}
